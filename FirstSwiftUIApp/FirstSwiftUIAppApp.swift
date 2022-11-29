cff9fa71b5a759c10fa6219db2b6e0ee3693f23a
serkanboyraci <serkanboyraci@gmail.com>
2022-11-29 23:37:27 +0300

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Ali serkan Boyracı  on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
